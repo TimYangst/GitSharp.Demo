7ec78dc680d054c06fa2e6512d0f5d4a0244884b
Ting Yang <t-tiyan@microsoft.com>
2013-07-31 18:15:00 +0800

    mod : reference

---
+++ GitSharp.Demo.Test/RepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitSharp.Demo.Test
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        [TestMethod]
+        public void TestRoadRepostory()
+        {
+            string project_path = "D:\\workspace\\leetcode";
+            var git_url = Repository.FindRepository(project_path);
+
+            var repo = new Repository(git_url);
+
+            Console.WriteLine(repo.Status.Untracked.Count);
+            foreach (var untrack in repo.Status.Untracked)
+            {
+                Console.WriteLine(untrack);
+            } 
+            //var head = repo.Head.Name;
+
+        }
+    }
+}
